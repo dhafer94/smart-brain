277a34d7c563206f4eebf946eaa23cc03c4aa78c
Dhafer Alzubairi <dx.dhafer@gmail.com>
2021-12-01 02:39:37 +0200

    added image link form with styling

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 
 function App() {
 	return (
 		<>
 			<Navigation />
 			<Logo />
-			{/* <ImageLinkForm />
-      <FaceRecognition /> */}
+			<ImageLinkForm />
+			{/* <FaceRecognition /> */}
 		</>
 	);
 }
